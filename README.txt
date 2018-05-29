7080a59ca0c26922da698dba6616a9e1e4afa922
Alexander Zent <azent@gmx.de>
2018-05-29 13:42:07 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+Run
+---
+ng serve
+... und im Browser aufrufen
+
+
+Dieses Eval-Projekt verdeutlicht die Einbindung eines Services per Factory-Funktion
+
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>
+    {{title}}
+  </h1>
+  `,
+  styles: [],
+  providers: [provideLoggingService()] // Provider wird durch eine Factory-Funktion bestimmt
 })
+
 export class AppComponent {
-  title = 'app';
+
+  // LoggingService wird injeziert
+  constructor(private logging: LoggingService){
+    logging.log('test log');
+  }
+
+  title = 'app works!';
+}
+
+export const LOGGING_USE_DATE = true;
+
+export function provideLoggingService() {
+  return {
+    provide: LoggingService,
+    useFactory: () => { 
+      return new LoggingService(LOGGING_USE_DATE) 
+    }
+  }
 }
